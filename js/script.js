38298829f25b624d0ee0fd89bdee629bf52b8feb
mahdimangeli1381 <mahdimangeligl@gmail.com>
2024-07-07 14:43:29 +0330

    change url

--- js/script.js
+++ js/script.js
@@ -369,7 +369,7 @@ window.addEventListener('load', () => {
     let owner = JSON.parse(localStorage.getItem('owner'));
     const baseUrl = window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost'
         ? ''
-        : '/USERS-CMS'
+        : 'USERS-CMS'
 
     if (!ownerId && (!owner || owner.length === 0) && !adminId) {
         location.href = `${baseUrl}/register.html`;
