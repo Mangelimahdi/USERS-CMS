b2fc8453978cf421ef7625d4282c10932136c90f
mahdimangeli1381 <mahdimangeligl@gmail.com>
2024-07-10 08:52:15 +0330

    fix bug  add id to input tags  users.html

--- js/users.js
+++ js/users.js
@@ -6,46 +6,52 @@ const supportCount = document.querySelector(".support-count");
 
 const getOwner = JSON.parse(localStorage.getItem('owner'));
 const getAdmins = JSON.parse(localStorage.getItem('admins'));
-console.log(getOwner)
+
 const ownerCounter = () => {
     if (getOwner || getOwner.length != 0) {
-        ownerCount.innerHTML = getOwner.length
+        ownerCount.innerHTML = getOwner.length;
     }
     else {
-        ownerCount.innerHTML = '0'
+        ownerCount.innerHTML = 0;
     }
 }
 
 const adminCounter = () => {
-    let adminFilter = getAdmins.filter(admin => {
-        return admin.roule === 'admin'
-    })
-    if (adminFilter) {
-        adminCount.innerHTML = adminFilter.length
-    } else {
-        adminCount.innerHTML = '0';
+    if(getAdmins){
+        let adminFilter = getAdmins.filter(admin => {
+            return admin.roule === 'admin'
+        })
+        if (adminFilter) {
+            adminCount.innerHTML = adminFilter.length
+        } else {
+            adminCount.innerHTML = '0';
+        }
     }
 }
 
 const employeeCounter = () => {
-    let employeeFilter = getAdmins.filter(employee => {
-        return employee.roule === 'employee';
-    });
-    if (employeeFilter) {
-        employeeCount.innerHTML = employeeFilter.length
-    } else {
-        employeeCount.innerHTML = '0'
+    if(getAdmins){
+        let employeeFilter = getAdmins.filter(employee => {
+            return employee.roule === 'employee';
+        });
+        if (employeeFilter) {
+            employeeCount.innerHTML = employeeFilter.length
+        } else {
+            employeeCount.innerHTML = "0"
+        }
     }
 }
 const supportCounter = () => {
-    let supportFilter = getAdmins.filter(support => {
-        return support.roule === 'support';
-    });
-    if (supportFilter) {
-        supportCount.innerHTML = supportFilter.length
-    }
-    else {
-        supportCount.innerHTML = '0'
+    if(getAdmins){
+        let supportFilter = getAdmins.filter(support => {
+            return support.roule === 'support';
+        });
+        if (supportFilter) {
+            supportCount.innerHTML = supportFilter.length
+        }
+        else {
+            supportCount.innerHTML = '0'
+        }
     }
 }
 ownerCounter();
